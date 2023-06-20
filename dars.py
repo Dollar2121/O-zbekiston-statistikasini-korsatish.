20a9f171a885dbc34beff2a0e2f2086f90b37dfc
Dollar2121 <muhammadfathullaev674@gmail.com>
2023-06-20 16:41:26 +0500

    First commit

---
+++ dars.py
@@ -0,0 +1,20 @@
+
+# # a = [1, 3, "a", 7, "g", 8, "q"]
+# # str_list = []
+# # number = 0
+# # for i in a:
+# #     if type (i) ==str:
+# #         str_list.append()
+# #         if type(i) == int:
+# #             number = i + number
+# #            print(str_list, number)
+
+# a = "salom hammaga yahshimisizlar guruxdagilar"
+# b = ["salom", "hammaga", 'yaxshimisizlar', "guruxdagilar"]
+
+# b = a.split()
+# d = len (b[1]) + len(b[-1])
+
+# print(len(b[1]),b[-1])
+# print(d)
+
